69809580fee24447c9f3f928da23011f5337a424
iDamina <daminailiya@gmail.com>
2024-02-02 02:25:02 +0100

    binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,10 +8,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	if (tree)
+		return (binary_tree_height(tree->left) -
+				binary_tree_height(tree->right));
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
 }
 
 /**
@@ -27,8 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
 	return ((left_height > right_height) ? left_height : right_height);
 
